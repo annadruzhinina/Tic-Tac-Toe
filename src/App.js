e891b6d60f9ed4a2975ff01a7e7a75fda78bff4e
anna <anna@rcbd.org>
2023-07-30 21:47:12 -0400

    Completed task

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
-import "./App.css";
+import styles from "./App.module.css";
 import GameBoard from "./components/GameTable";
 
-function App() {
+function App({}) {
+  const name = "Anna";
   return (
-    <div className="container">
-      <h1 className="title">Tic-Tac-Toe</h1>
-      <GameBoard />
+    <div className={styles.container}>
+      <h2 className={styles.title}>Tic-Tac-Toe</h2>
+      <GameBoard name={name} />
     </div>
   );
 }
